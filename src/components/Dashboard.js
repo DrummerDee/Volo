d4a2ce71ba476c1904f4c751aa1b98a893fd28f6
saira512dev <512saira@gmail.com>
2022-10-19 10:00:05 +0530

    Added app bar to dashboard and made it responsive

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,55 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import MenuBar from "./MenuBar";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("VoloUser"));
 
-  const user = JSON.parse(localStorage.getItem('VoloUser'))
-  const handleSubmit = () => {
-
-    const response = fetch("http://localhost:1212/logout", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    localStorage.removeItem("VoloLoggedIn");
-    localStorage.removeItem("VoloUser");
-    navigate("/login");
-  };
 
   return (
-    <div class="container">
-<<<<<<< HEAD
-<header>
-=======
-      <div class="main">
+    <div className="dashboard">
       <header>
->>>>>>> origin/latest
-        <div>
-          <h1>Welcome {user.userName}</h1>
-          <input
-            onClick={handleSubmit}
-            type="submit"
-            value="Log Out"
-            id="log__out"
-          />
-        </div>
+        <MenuBar />
       </header>
-<<<<<<< HEAD
-      <main>
-=======
->>>>>>> origin/latest
-        <section className="containerleft">
-          <h2> Let's make a list... Together </h2>
-          <div className="leftwrapper"></div>
-        </section>
-<<<<<<< HEAD
-      </main>
-=======
+      <div className="container">
+        <div className="main">
+          <section className="containerleft">
+            <div>
+              <h1>Welcome {user.userName}</h1>
+            
+            </div>
+            <h2> Let's make a list... Together </h2>
+            <div className="leftwrapper"></div>
+          </section>
+        </div>
       </div>
-
->>>>>>> origin/latest
     </div>
   );
 };
