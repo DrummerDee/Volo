bd921d0172f1a5b47776656ccdb2b721ade24bd3
Drummer Dee <drummer.deja@gmail.com>
2022-10-21 19:56:11 -0400

    deployment

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -5,9 +5,26 @@ const Settings = () => {
   return (
     <>
         <MenuBar/>
-        <div> 
             <h1> Settings </h1>
+            <div> 
+                <label htmlFor='avatar-img'> Update Icon </label>
+                </div>
+            <div> 
+                <label htmlFor='update-username'> Update Username </label>
+                <input type='text' name='update-username'/>
+                <button> Update Username </button>
             </div>
+            <div>
+                <label htmlFor='update-password'> Update Password </label>
+                <input type='text' name="update-password" id="" />
+                <button> Change Password </button>
+                </div>
+                <div>
+                    <label htmlFor='change-email'> Update Email </label>
+                    <input type='email' name="update-email" id="" />
+                    <button> Change Email </button>
+                </div>
+                <button> Delete Account </button>
     </>
   )
 }
